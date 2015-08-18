0e38d9aadea40357da654c274e4b9a53df7363d8
George.Aidonidis <agiorgos@imgzine.com>
2015-08-18 17:14:28 +0300

    Added HelperMethods for encoding/decoding. Added basic MainPage. Added MainPage ViewModel. Added RelayCommandT class

---
+++ Base64Universal/Base64Universal/Base64Universal.Shared/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Base64Universal.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private string decodedText;
+        public string DecodedText
+        {
+            get { return decodedText; }
+            set
+            {
+                if (value != decodedText)
+                {
+                    decodedText = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string encodedText;
+        public string EncodedText
+        {
+            get { return encodedText; }
+            set
+            {
+                if (value != encodedText)
+                {
+                    encodedText = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+    }
+}
